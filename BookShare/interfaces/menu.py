03b5ffdb632517e6bff19ebdfe0aa0897da95b75
DgDavi <davi46796@gmail.com>
2026-03-29 16:19:08 -0300

    Feat: Criei a pagina de informações sobre o projeto.

--- BookShare/interfaces/menu.py
+++ BookShare/interfaces/menu.py
@@ -1,5 +1,6 @@
-from utils.limpar_tela import limpar_tela
 from colorama import Fore
+
+from utils.limpar_tela import limpar_tela
 from services.auth_services import cadastrar_usuario, login_usuario
 
 
@@ -33,7 +34,7 @@ def menu_inical():
         login_usuario()
 
     elif opcao == 3:
-        print("A fazer")
+        info_menu()
 
     # Fecha a aplicação
     elif opcao == 0:
@@ -86,3 +87,35 @@ def menu_usuario():
         print("Saindo do aplicativo...")
         limpar_tela()
         exit()
+    
+    else:
+        print(Fore.RED + "❌ Opção inválida. Tente novamente.")
+        menu_usuario()
+
+
+# Menu de informações sobre o projeto
+def info_menu():
+    limpar_tela()
+
+    print(Fore.CYAN + "="*60)
+    print(Fore.CYAN + "📚 SOBRE O PROJETO".center(60))
+    print(Fore.CYAN + "="*60)
+
+    print()
+    print("""O BookShare é um aplicativo de compartilhamento de livros desenvolvido como parte da disciplina de Projeto Interdisciplinar para Sistemas de Informação I. O projeto foi idealizado e criado por Davi Gomes e Lucas Augusto com o objetivo de aplicar, na prática, conceitos aprendidos ao longo do curso, unindo tecnologia e impacto social.
+
+A proposta do BookShare é permitir que usuários cadastrem seus livros, procurem por títulos disponíveis e compartilhem suas leituras com outros membros da comunidade. Dessa forma, o sistema busca promover a troca de conhecimento, incentivar o hábito da leitura e facilitar o acesso a diferentes obras de maneira colaborativa.
+
+Mais do que um simples sistema, o BookShare representa a ideia de que o conhecimento deve ser acessível e compartilhado, fortalecendo a conexão entre pessoas por meio da leitura.
+
+🔗 LinkedIn dos criadores:
+Davi Gomes: https://www.linkedin.com/in/davigomes1/
+Lucas Augusto: (colocar link)
+
+💻 GitHub dos criadores:
+Davi Gomes: https://github.com/DgDavi
+Lucas Augusto: (colocar link)""")
+
+    print()
+    input(Fore.GREEN + "Pressione qualquer tecla para voltar ao menu principal...")
+    menu_inical()
