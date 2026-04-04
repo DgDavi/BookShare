5499b1aecec3c7c984b519ebf75eb2cdcdd45710
DgDavi <davi46796@gmail.com>
2026-04-03 23:51:49 -0300

    Removi os comentario de auth_services

--- BookShare/app/services/auth_services.py
+++ BookShare/app/services/auth_services.py
@@ -16,41 +16,12 @@ def cadastrar_usuario():
     print(Fore.YELLOW + "📋 CADASTRO DE USUÁRIO\n" + Fore.CYAN + "-"*30)
 
     
-    # conexao = get_db_connection()
-    # cursor = conexao.cursor()
-
-    # Validação do nome
-    # while True:
-    #     nome = input("\nDigite seu nome: ")
-    #     if len(nome) > 50 or len(nome) < 3:
-    #         print(Fore.RED + " ❌ O nome deve conter entre 3 e 50 caracteres.")
-    #         print(Fore.YELLOW + "👉 Tente novamente.")
-    #     else:
-    #         break
     nome = validar_input(
         Fore.YELLOW + "👉 Digite seu nome: ",
         lambda n: 3 <= len(n) <= 50,
         Fore.RED + "❌ O nome deve conter entre 3 e 50 caracteres.\n" + Fore.YELLOW + "👉 Tente novamente."
     )
         
-    # Validação do email
-    # while True:
-    #     email = input("Digite seu email: ")
-
-    #     # Verifica se o email já existe no banco de dados
-    #     cursor.execute("SELECT EXISTS(SELECT 1 FROM usuarios WHERE email = ?)", (email,))
-
-    #     if cursor.fetchone()[0] == 1:
-    #         print(Fore.RED + " ❌ Email já cadastrado.")
-    #         print(Fore.YELLOW + "👉 Tente novamente.")
-    #         continue
-
-    #     # Verifica se o email tem um formato válido
-    #     if validar_email(email):
-    #         break
-    #     else:
-    #         print(Fore.RED + " ❌ Formatação de email inválida.")
-    #         print(Fore.YELLOW + "👉 Tente novamente.")
 
     email = validar_input(
         Fore.YELLOW + "👉 Digite seu email: ",
@@ -58,21 +29,6 @@ def cadastrar_usuario():
         ""
     )
 
-    # Validação da senha
-    # while True:
-    #     senha = input("Digite sua senha: ")
-    #     if not validar_senha(senha):
-    #         print(Fore.RED + " ❌ A senha deve conter pelo menos 8 caracteres, incluindo uma letra maiúscula, um número e um caractere especial.")
-    #         print(Fore.YELLOW + "👉 Tente novamente.")
-
-    #     # Confirmação da senha
-    #     confirmacao_senha = input("Confirme sua senha: ")
-    #     if senha != confirmacao_senha:
-    #         print(Fore.RED + " ❌ As senhas não coincidem.")
-    #         print(Fore.YELLOW + "👉 Tente novamente."   )
-    #     else:
-    #         senha_hashed = hash_senha(senha)
-    #         break
 
     senha = validar_input_senha()
     senha_hashed = hash_senha(senha)
@@ -97,17 +53,6 @@ def login_usuario():
     limpar_tela()
     print(Fore.YELLOW + "📋 LOGIN DE USUÁRIO\n" + Fore.CYAN + "-"*30)
 
-    # while True:
-        
-    #     email = input("Digite seu email: ")
-
-    #     # Verifica se o email existe no banco de dados
-    #     cursor.execute("SELECT EXISTS(SELECT 1 FROM usuarios WHERE email = ?)", (email,))
-    #     if cursor.fetchone()[0] == 1:
-    #         break
-    #     else:
-    #         print(Fore.RED + " ❌ Email não encontrado.")
-    #         print(Fore.YELLOW + "👉 Tente novamente.")
 
     email = validar_input(
         Fore.YELLOW + "👉 Digite seu email: ",
@@ -115,21 +60,6 @@ def login_usuario():
         ""
     )
 
-
-    # while True:
-    #     senha = input("Digite sua senha: ")
-
-    #     # Transforma a senha digitada em hash e compara com a senha armazenada no banco de dados para aquele email
-    #     senha_hashed = hash_senha(senha)
-    #     cursor.execute("SELECT senha FROM usuarios WHERE email = ?", (email,))
-    #     senha_armazenada = cursor.fetchone()[0]
-    #     if senha_hashed == senha_armazenada:
-    #         print(Fore.GREEN + "Login bem-sucedido!")
-    #         input(Fore.CYAN + "Pressione Enter para continuar...")
-    #         break
-    #     else:
-    #         print(Fore.RED + " ❌ Senha incorreta.")
-    #         print(Fore.YELLOW + "👉 Tente novamente.")
 
     senha = validar_input(
         Fore.YELLOW + "👉 Digite sua senha: ",
