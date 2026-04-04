85e3b126daf254fb694866e1727223a929848d5f
DgDavi <davi46796@gmail.com>
2026-04-03 23:44:18 -0300

    Eu tinha feito merda mas corrigi.

--- BookShare/data/db.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-# Função para obter uma conexão com o banco de dados
-def get_db_connection():
-    conexao = sqlite3.connect('usuarios.db')
-    conexao.row_factory = sqlite3.Row
-    return conexao
--- BookShare/utils/security.py
+++
@@ -1,5 +0,0 @@
-import hashlib
-
-# Função que criptografa a senha usando SHA-256
-def hash_senha(senha):
-    return hashlib.sha256(senha.encode()).hexdigest()
