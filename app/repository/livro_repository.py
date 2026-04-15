e6c8ec1b90d51593cb98bc7d780ed41adf7fbf2c
luquetaaasn <lucasaugustoseabranunes@gmail.com>
2026-04-15 18:18:48 -0300

    funçoes de buscar_livro, editar_titulo, editar_descricao e deletar_livro

--- app/repository/livro_repository.py
+++ app/repository/livro_repository.py
@@ -5,3 +5,60 @@ def criar_livro(livro, cursor):
     )
     livro.id = cursor.lastrowid
     return livro
+
+
+
+def buscar_livros(usuario, cursor):
+    cursor.execute(
+        "SELECT id, user_id, titulo, descricao FROM livros WHERE user_id = ?",
+        (usuario.id,)
+    )
+
+    return cursor.fetchall()
+
+def editar_titulo(livro, titulo, cursor):
+    cursor.execute(
+        "UPDATE livros SET titulo = ? WHERE id = ?",
+        (titulo, livro.id)
+    )
+
+    livro.titulo = titulo
+
+    cursor.execute(
+        "SELECT titulo FROM livros WHERE id = ?",
+        (livro.id,)
+    )
+    resultado = cursor.fetchone()
+
+    if resultado is None:
+        return False
+
+    novo_titulo = resultado[0]
+    return novo_titulo == livro.titulo
+
+def editar_descricao(livro, descricao, cursor):
+    cursor.execute(
+        "UPDATE livros SET descricao = ? WHERE id = ?",
+        (descricao, livro.id)
+    )
+
+    livro.descricao = descricao
+
+    cursor.execute(
+        "SELECT descricao FROM livros WHERE id = ?",
+        (livro.id,)
+    )
+    resultado = cursor.fetchone()
+
+    if resultado is None:
+        return False
+
+    nova_descricao = resultado[0]
+    return nova_descricao == livro.descricao
+
+def deletar_livro(livro, cursor):
+    cursor.execute(
+        "DELETE FROM livros WHERE id = ?",
+        (livro.id,)
+    )
+    return True
